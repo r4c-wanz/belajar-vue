dcc6ae634c875207014a29bedfb13f911adea5f3
R4C-Wanz <rahelsetyawan321@gmail.com>
2023-01-03 07:33:42 +0700

    Belajar npm

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1 :title="messages">Halo Vue</h1>
+        <p>{{messages}}</p>
+        <button @click="ubahKata()">Ubah Kata</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                messages: "Meledak"
+            }
+        },
+        methods: {
+            ubahKata(){
+                this.messages = 'Mantapp'
+            }
+        }
+    }
+</script>
+
+<style>
+    
+</style>
