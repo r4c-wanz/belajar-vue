76230b82e6142fd44fec49a42c643b5cd18198c5
Me <103583267+R4C-Wanz@users.noreply.github.com>
2023-01-09 10:20:05 +0700

    Belajar-Vue

--- src/pages/tutorial/04Lifecycle.vue
+++ src/pages/tutorial/04Lifecycle.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <h1>Tutorial Lifecycle</h1>
+        <p id="textnya">Haloooo</p>
     </div>
 </template>
 
@@ -12,12 +13,18 @@
             }
         },
         created() {
+            // Dijalanin pas layar masih putih / loading || Rekomendasi ubah data
+            console.log('Created')
             console.log(this.nama)
             this.nama = 'Bang Jono'
             console.log(this.nama)
         },
         mounted() {
-            
+            // Dijalanin pas HTML muncul
+            console.log('Mounted')
+            console.log(this.nama)
+            this.nama = 'Bang Juan'
+            console.log(this.nama)
         }
     }
 </script>
