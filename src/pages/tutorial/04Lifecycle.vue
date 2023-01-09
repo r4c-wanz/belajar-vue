caca04db8362f30cade3ff8a73b59f8eaf505bdc
Me <103583267+R4C-Wanz@users.noreply.github.com>
2023-01-09 10:13:43 +0700

    Learn D-Flex

--- src/pages/tutorial/04Lifecycle.vue
+++ src/pages/tutorial/04Lifecycle.vue
@@ -3,3 +3,21 @@
         <h1>Tutorial Lifecycle</h1>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                nama: 'Bang Messi'
+            }
+        },
+        created() {
+            console.log(this.nama)
+            this.nama = 'Bang Jono'
+            console.log(this.nama)
+        },
+        mounted() {
+            
+        }
+    }
+</script>
